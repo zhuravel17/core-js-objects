96a4789c421990d2d3d0609141cb62f7826ac826
zhuravel17 <zhuravlevakat1996@gmail.com>
2025-03-10 17:54:50 +0400

    feat: add Rectangle

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -167,8 +167,12 @@ function sellTickets(queue) {
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  this.width = width;
+  this.height = height;
+  this.getArea = function () {
+    return this.width * this.height;
+  };
 }
 
 /**
